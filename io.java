3277bac03fcc967947589581cd8aabc77968aad0
vignesh7rb <155147285+vignesh7rb@users.noreply.github.com>
2025-11-21 13:23:16 +0530

    Create io.java

---
+++ io.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class main(){
+  public static void main(string[] args){
+    Scanner sc = new Scanner(System.in);
+    int a = sc.nextInt();
+    int b = sc.next();
+    int c = sc.nextLine();
+    System.out.print(a);
+    System.out.print(b);
+    System.out.print(c);
+    sc.close();
+  }
+}
+
